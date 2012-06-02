4bd172df4f10aca074bac2f9f6fb637a1464370a
Robert Greiner <robert@robertgreiner.com>
2012-06-02 07:27:09 -0500

    Initial commit

---
+++ Azure.ACS/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Azure.ACS.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            var identity = HttpContext.User.Identity as Microsoft.IdentityModel.Claims.IClaimsIdentity;
+
+            ViewBag.NameIdentifier = "";
+            if (identity != null)
+            {
+                ViewBag.NameIdentifier = identity.Claims.First(x => x.ClaimType.Contains("nameidentifier")).Value;
+            }
+
+            return View();
+        }
+    }
+}
---
+++ readme.md
@@ -0,0 +1,11 @@
+Azure.ACS
+===========
+
+Uses Windows Azure Access Control Service to log a user in with a Windows Live ID and displays their claims nameidentifier once logged in.
+
+Copyright
+---------
+
+Copyright (c) 2012 <a href="http://creatingcode.com">Robert Greiner</a>. 
+
+git-ignoreis released under the <a href="http://ospl.ws">Open Source Public License</a>. See LICENSE for details.
